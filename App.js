251631d99ab45584ad73741423e5d41708203c80
MikhailPakhomov <mihail93@mail.ru>
2024-04-26 15:12:14 +0300

    feat: add Map, add preloader

--- App.js
+++ App.js
@@ -1,13 +1,72 @@
 import { StyleSheet, Text, View } from 'react-native';
 import Coordinates from './components/Coordinates';
+import MapView from 'react-native-maps';
+import { useState, useEffect } from 'react'
+import * as Location from 'expo-location';
+
 
 export default function App() {
+  const [location, setLocation] = useState({
+    "coords": {
+      "latitude": null,
+      "longitude": null
+    }
+  });
+  let currentLocation;
+  let isShowUserLocation = true;
 
+  useEffect(() => {
+    let interval;
+    const getPermissions = async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        getPermissions();
+        return;
+      }
+      interval = setInterval(async () => {
+        currentLocation = await Location.getCurrentPositionAsync({});
+        setLocation(currentLocation);
+      }, 1000);
+    };
+    getPermissions();
+
+    return () => {
+      clearInterval(interval)
+    }
+  }, []);
+
+
+  console.log('Широта:' + JSON.stringify(location))
+  console.log(isShowUserLocation)
+  // console.log('Долгота:' + location.coords.longitude)
   return (
     <>
-      <Coordinates />
-    </>
+      {(location.coords.latitude === null) ?
+        <View style={styles.container}>
+          <Text style={styles.preloader}>Запуск...</Text>
+        </View>
+        :
+        <View style={styles.container}>
+          <Coordinates
+            latitude={location.coords.latitude}
+            longitude={location.coords.longitude}
+          />
+          <MapView style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.01,
+              longitudeDelta: 0.01
+            }}
+            showsUserLocation={isShowUserLocation}
+            showsMyLocationButton={true}
+            userLocationUpdateInterval={1000}
+          />
+        </View>
 
+      }
+    </>
   );
 }
 
@@ -18,4 +77,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '85%',
+  },
+  preloader: {
+    fontSize: 32,
+  }
+
 });
